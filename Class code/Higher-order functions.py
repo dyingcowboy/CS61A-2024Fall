9d527aac0370d470fa849c3b29df4e2efe07262c
John Xueshi <2313421286@qq.com>
2025-07-02 00:15:05 +0800

    Fifth

--- Class code/Higher-order functions.py
+++ Class code/Higher-order functions.py
@@ -19,3 +19,41 @@ def are_circle(r):
 def area_hexagon(r):
     return area(r,3*sqrt(3)/2)
 
+# def sum_naturals(n):
+#     """Sum thr first N batural numbers
+#     >>> sum_naturals(5)
+#     15
+#     """
+#     total ,k=0,1
+#     while k<=n:
+#         total,k=total+k,k+1
+#     return total
+#     use the higher-order function!
+
+def identity(k):
+    return k
+
+def cube(k):
+    return pow(k,3)
+
+def summation(n,term):
+    total,k=0,1
+    while k<=n:
+        total,k=total+term(k),k+1
+    return total
+
+# functions as return values
+
+def make_addder(n):
+    """Return a function that takes one argument called k and return k+n
+    >>> add_three=make_adder(3)
+    >>> add_three(4)
+    7
+    """
+    def adder(k):
+        return k+n
+    return adder
+
+
+
+
